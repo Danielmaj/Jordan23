8ec70ae10bb8c59b3225d0f6c867e0585a504d9f
aqeel13932 <aqeel.labash@gmail.com>
2018-09-23 17:05:01 +0300

    update main to hanlde the new changes

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 import serial
-from Comunication.mainboard import ComportMainboard
+from Hardware.mainboard import ComportMainboard
 from time import sleep
 from Motor.direction import *
 from Vision.locateball import *
@@ -15,55 +15,6 @@ def move(vel):
     com.write("sd:{}:{}:{}:{}".format(vel[0],vel[3],vel[2],vel[3]))
     #print(com.connection.readline())
     #com.connection.flush()
-
-def move_constant(vel):
-    while true
-        move(vel)
-        time.sleep(0.08)
-
-
-def Centerball():
-    ''' Rotate until you find the green ball around center camera'''
-
-    com = CreateConnection()
-    com.launch_motor(100)
-
-    pool = Pool(processes=2)
-    see_ball = pool.map_async(LocateGreenBall)
-    velrot = test_all_wheels(20)
-    rotate = pool.map_async(move_constant,velrot)
-
-    # Stop
-    see_ball.wait()
-    pool.close()
-    pool.join()
-    move(stop())
-
-def GoToballStraight():
-    '''Robot drives straight to the ball and does not rotate '''
-    Centerball()
-
-    #pool = Pool(processes=2)
-    #see_ball = pool.map_async(LocateGreenBall)
-    #vel = forward(30)
-    #rotate = pool.map_async(move_constant,velrot)
-
-    # Stop
-    #see_ball.wait()
-    #pool.close()
-    #pool.join()
-    #move(stop())
-    #com.close()
-
-
-
-def GotoBallAngle():
-    '''Robot drives at an angle to the ball and does not rotate '''
-    move(stop())
-
-
-
-
 
 def main():
     com =CreateConnection()
