82853272345e137b4a82fa3d82c17389312e3b42
aqeel13932 <aqeel.labash@gmail.com>
2018-09-23 20:29:45 +0300

    remove uneeded code

--- Main.py
+++ Main.py
@@ -12,34 +12,16 @@ def CreateConnection():
     com.open()
     return com
 
-def Readmsgs(com):
-    try:
-       com.connection.flush()
-       print(com.connection.readline())
-    except Exception as ex:
-       print(ex)
-
-def move(com,vel):
-    com.write("sd:{}:{}:{}".format(vel[0],vel[1],vel[2]))
-    Readmsgs(com)
-
-def thrower(com,value):
-    com.write("d:{}".format(value))
-    Readmsgs(com)
 def Test_motors(com):
-    #thrower(com,1500)
+    thrower(com,1500)
     move(com,stop())
     for i in range(3):
-       move(com,right(30))
+       move(com,left(10))
        sleep(1)
     move(com,stop())
 
-    for i in range(3):
-       move(com,left(30))
-       sleep(1)
 def main():
     com =CreateConnection()
-    com.launch_motor(100)
     Test_motors(com)
     com.close()
 main()
