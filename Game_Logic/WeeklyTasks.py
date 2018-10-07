243da4a5bb3b7a8846bc550de98442db002b7659
Danielmaj <dnlmjrl@gmail.com>
2018-10-07 11:44:49 +0300

    alignment bertween ball and Basket

---
+++ Game_Logic/WeeklyTasks.py
@@ -0,0 +1,232 @@
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+import sys
+sys.path.append('../')
+from Image_Processing.Image_Handler import Image_Handler
+from Hardware.mainboard import *
+from Hardware.Motor import *
+from config import Config
+from time import sleep
+
+def LocateBasket(frame,color,config):
+
+    if color == "blue":
+        low = lower_blue
+        up = upper_blue
+    else:
+        low = lower_magenta
+        up = upper_magenta
+
+
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, low, up)
+    mask = cv2.erode(mask, None, iterations=1)
+    mask = cv2.dilate(mask, None, iterations=1)
+
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                            cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+    if len(cnts) > 0:
+
+        c = max(cnts, key=cv2.contourArea)
+        (x, y), (width, height), rect_angle = cv2.minAreaRect(c)
+
+        # only proceed if the basket meets a minimum size
+        if width*height > 6:
+            center = (x, y)
+    return center
+
+
+def Start_Pipeline(config):
+
+    # Configure depth and color streams
+    pipeline = rs.pipeline()
+    rsconfig = rs.config()
+    rsconfig.enable_stream(rs.stream.depth, config.frame_width, config.frame_heigth, rs.format.z16, 30)
+    rsconfig.enable_stream(rs.stream.color, config.frame_width, config.frame_heigth, rs.format.bgr8, 30)
+    # Start streaming
+    pipeline.start(rsconfig)
+
+    return pipeline
+
+def Get_frames(pipeline):
+    # Wait for a coherent pair of frames: depth and color
+    while True:
+        frames = pipeline.wait_for_frames()
+        depth_frame = frames.get_depth_frame()
+        color_frame = frames.get_color_frame()
+        if not depth_frame or not color_frame:
+            continue
+        else:
+            break
+    return color_frame,depth_frame
+
+
+def Rotate_towards(obj_name,com,coordinates,config):
+    x,y = coordinates
+    centred = False
+    if x > config.max_cent_x:
+       move(com,right(config.cent_vel))
+    elif x < config.min_cent_x:
+       move(com,left(config.cent_vel))
+    else:
+       print('Centered on:',obj_name)
+       move(com,stop())
+       centred = True
+    return centred
+
+
+def Where_is(obj_name,color_frame,config):
+
+    coordinates = None
+    color_image = np.asanyarray(color_frame.get_data())
+
+    if obj_name == 'ball':
+        coordinates = img_handler.LocateBallCenter(color_image)
+    else:
+        coordinates = LocateBasket(color_image,"blue",config)
+
+    return coordinates
+
+def Rotate_to_see(obj_name,color_frame,config):
+
+    coordinates = None
+    color_image = np.asanyarray(color_frame.get_data())
+
+    coordinates = Where_is('ball',color_frame,config)
+
+    if coordinates == None:
+        move(com,left(rot_vel))
+
+    return coordinates
+
+
+def CenterOn(obj_name,com,pipeline,config):
+
+    centered = False
+
+    while not centered:
+
+        color_frame,depth_frame = Get_frames(pipeline)
+        coordinates = Where_is(obj_name,color_frame,img_handler,config)
+
+        if coordinates == None:
+            rotate+=1
+            if rotate < config.rot_wait:
+                move(com,left(rot_vel))
+                time.sleep(config.wait_time)
+                rotate = 0
+                coordinates = Where_is(obj_name,color_frame,img_handler,config)
+        else:
+            centered = Rotate_towards(obj_name,com,color_frame,config)
+
+
+def GoTowards(obj_name,com,pipeline,config,until,vel):
+
+    near = False
+    while not near:
+
+        color_frame,depth_frame = Get_frames(pipeline)
+        coordinates = Where_is(obj_name,color_frame,img_handler,config)
+
+        if coordinates == None:
+            CenterOn(obj_name,com,pipeline,config)
+        else:
+            x,y = coordinates
+            dist = depth_frame.get_distance(int(x),int(y))
+            if dist > until:
+                move(com,wheelspeeds(vel,90,0))
+            elif dist > 0.1:
+                move(com,stop())
+            time.sleep(config.wait_time)
+
+
+def ang_vel_towards(obj_name,com,coordinates,config):
+    x,y = coordinates
+    if x > config.max_cent_x:
+       ang_vel = around_ang_vel
+    elif x < config.min_cent_x:
+       ang_vel = - around_ang_vel
+    else:
+       print('Centered on:',obj_name)
+    return ang_vel
+
+
+def Aling_Basket_Ball(com,coordinates,pipeline,config):
+    #Todo add constants to config
+    align = False
+
+    while not align:
+
+        color_frame,depth_frame = Get_frames(pipeline)
+        coordinates_basket = Where_is("basket",color_frame,img_handler,config)
+        coordinates_ball = Where_is("ball",color_frame,img_handler,config)
+        xk,yk = coordinates_basket
+
+        if coordinates_ball == None:
+            CenterOn('ball',com,pipeline,config)
+            GoTowards('ball',com,pipeline,config,until=0.4,vel=15)
+
+        else:
+
+            xb,yb = coordinates_ball
+
+            if xb > config.max_cent_x:
+                ang_vel = config.around_ang_vel
+            elif xb < config.min_cent_x:
+               ang_vel = config.around_ang_vel
+            else:
+               ang_vel = 0
+
+            if coordinates_basket == None:
+
+                move(com,wheelspeeds(10,180,ang_vel))
+                time.sleep(config.wait_time)
+
+            else:
+
+                xbk,ybk = coordinates_basket
+
+                if xbk > config.max_cent_x:
+                    move(com,wheelspeeds(5,180,ang_vel))
+                elif xbk < config.min_cent_x:
+                    move(com,wheelspeeds(5,0,ang_vel))
+                else:
+                   move(com,stop())
+                   align = True
+
+                time.sleep(config.wait_time)
+
+
+def main():
+
+    com = ComportMainboard()
+    com.open()
+
+    config = Config()
+    pipeline = Start_Pipeline(config)
+    img_handler = Image_Handler()
+
+    steps_forward = 20
+
+    for i in range(steps_forward):
+        move(com,wheelspeeds(15,90,0))
+        time.sleep(config.wait_time)
+
+    CenterOn('ball',com,pipeline,config)
+
+    GoTowards('ball',com,pipeline,config,until=0.4,vel=15)
+
+    Aling_Basket_Ball(com,coordinates,pipeline,config)
+
+    GoTowards('ball',com,pipeline,until=0.2,vel=5)
+
+    #Launch the ball
+    com.launch_motor(self, 2000)
+
+    pipeline.stop()
+    com.close()
+
+main()
---
+++ config.py
@@ -0,0 +1,72 @@
+"""
+Jordan 23
+Base Configurations class.
+"""
+import math
+import numpy as np
+
+# Base Configuration Class
+# Don't use this class directly to try things. Instead, sub-class it and override
+# the configurations you need to change.
+
+class Config(object):
+    """Base configuration class. To try configurations, create a
+    sub-class that inherits from this one and override properties
+    that need to be changed.
+    """
+    # Name the configurations. For example "Week 4 Task "
+    # Useful if your code needs to do things differently depending on which
+    # experiment is running.
+    NAME = 'basic'  # Override in sub-classes
+
+    # Image form camera width and height
+    frame_width = 640
+    frame_heigth = 480
+
+    # Center of robot in the frame
+    robot_center_x = 370
+
+    # How many frames to wait before starting to rotate again to locate some object
+    rot_wait =  10
+
+    # velocity of Rotation to locate object
+    rot_vel =  20
+
+    # Precision of rotation centering on object. Error allowed in pixels when rotating centering on an object
+    cent_precision = 15
+
+    # Velocity of centering on an object
+    cent_vel = 5
+
+    # Time to sleep between motor comands to avoid too many comands
+    wait_time = 0.1
+
+    # Moving forward starting from the corner in omniwheel coordinates (velocity,angle,angular velocity)
+    corner_omni_vel = np.array([15, 90, 0])
+
+    # Parameters to rotate around the Ball
+    around_ang_vel = 10
+
+    #Parameters to locate the Basket
+    # basket blue
+    lower_blue = np.array([97, 100, 0])
+    upper_blue = np.array([116, 255, 255])
+
+    # basket magenta
+    lower_magenta = np.array([169, 130, 105])
+    upper_magenta = np.array([180, 255, 255])
+
+
+    def __init__(self):
+        """Set values of computed attributes."""
+        # Maximum x to consider the robot centred respect to an object
+        max_cent_x = robot_center_x + cent_precision
+        min_cent_x = robot_center_x - cent_precision
+
+    def display(self):
+        """Display Configuration values."""
+        print("\nConfigurations:")
+        for a in dir(self):
+            if not a.startswith("__") and not callable(getattr(self, a)):
+                print("{:30} {}".format(a, getattr(self, a)))
+        print("\n")
