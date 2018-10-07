corrected sign velocity

config.py
@@ -54,7 +54,7 @@ class Config(object):
     #Parameters to locate the Basket
     # basket blue
     lower_blue = np.array([95, 160, 100])
-    upper_blue = np.array([105, 255, 158])
+    upper_blue = np.array([105, 255, 130])
 
     # basket magenta
     lower_magenta = np.array([169, 130, 105])